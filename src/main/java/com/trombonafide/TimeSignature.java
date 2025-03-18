cad63d9261f2be4097589ef6bd2a74597ff8a88b
John Mai <143438343+JohnM312@users.noreply.github.com>
2025-03-18 01:24:18 -0400

    Create TimeSignature.java

---
+++ src/main/java/com/trombonafide/TimeSignature.java
@@ -0,0 +1,17 @@
+public class TimeSignature {
+    private int numerator;
+    private int denominator;
+
+    public TimeSignature(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
+
+    public int getBeatsPerMeasure() {
+        return numerator;
+    }
+
+    public boolean isCompound() {
+        return numerator % 3 == 0;
+    }
+}
