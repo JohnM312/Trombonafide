d52113edfd0a508061c51d810ac42d856591a8d4
John Mai <143438343+JohnM312@users.noreply.github.com>
2025-03-24 15:41:38 -0400

    Update Chord.java

--- src/main/java/com/trombonafide/Chord.java
+++ src/main/java/com/trombonafide/Chord.java
@@ -2,12 +2,25 @@ package com.trombonafide;
 
 import java.util.List;
 
+/**
+ * Represents a musical chord, consisting of multiple notes played together.
+ * 
+ * @author John Mai
+ */
 public class Chord {
     private String name;
     private List<Note> notes;
     private String quality;
     private int inversion;
 
+    /**
+     * Constructs a new Chord instance.
+     *
+     * @param name the name of the chord
+     * @param notes the list of notes forming the chord
+     * @param quality the chord quality (e.g., major, minor)
+     * @param inversion the inversion level of the chord
+     */
     public Chord(String name, List<Note> notes, String quality, int inversion) {
         this.name = name;
         this.notes = notes;
@@ -15,16 +28,29 @@ public class Chord {
         this.inversion = inversion;
     }
 
+    /**
+     * Plays the chord.
+     */
     public void play() {
         // Play the chord
     }
 
+    /**
+     * Transposes the chord by a given interval.
+     *
+     * @param interval the interval by which to transpose the chord
+     */
     public void transpose(int interval) {
         for (Note note : notes) {
             note.transpose(interval);
         }
     }
 
+    /**
+     * Gets the list of notes forming the chord.
+     *
+     * @return the list of notes
+     */
     public List<Note> getNotes() {
         return notes;
     }
