d938bd363d01ac7d3812a8efd1ddedd3e8e18ee1
John Mai <143438343+JohnM312@users.noreply.github.com>
2025-03-24 15:42:38 -0400

    Update KeySignature.java

--- src/main/java/com/trombonafide/KeySignature.java
+++ src/main/java/com/trombonafide/KeySignature.java
@@ -2,21 +2,42 @@ package com.trombonafide;
 
 import java.util.List;
 
+/**
+ * Represents the key signature of a piece of music, including accidentals.
+ * Supports transposition and relative minor identification.
+ * 
+ * @author John Mai
+ */
 public class KeySignature {
     private String key;
     private List<String> accidentals;
 
+    /**
+     * Constructs a new KeySignature instance.
+     *
+     * @param key the key of the music (e.g., "C Major", "G Minor")
+     * @param accidentals the list of accidentals present in the key signature
+     */
     public KeySignature(String key, List<String> accidentals) {
         this.key = key;
         this.accidentals = accidentals;
     }
 
+    /**
+     * Transposes the key signature by a given interval.
+     *
+     * @param interval the interval by which to transpose
+     */
     public void transpose(int interval) {
         // Transpose key signature
     }
 
+    /**
+     * Gets the relative minor key.
+     *
+     * @return the relative minor key as a string
+     */
     public String getRelativeMinor() {
-        // Return relative minor key
         return "";
     }
 }
