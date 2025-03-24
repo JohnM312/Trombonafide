aed32021e68792ba664d2cc99f45e80b20345b30
John Mai <143438343+JohnM312@users.noreply.github.com>
2025-03-24 15:42:21 -0400

    Update TimeSignature.java

--- src/main/java/com/trombonafide/TimeSignature.java
+++ src/main/java/com/trombonafide/TimeSignature.java
@@ -1,18 +1,39 @@
 package com.trombonafide;
 
+/**
+ * Represents the time signature of a piece of music, defining beats per measure.
+ * 
+ * @author John Mai
+ */
 public class TimeSignature {
     private int numerator;
     private int denominator;
 
+    /**
+     * Constructs a new TimeSignature instance.
+     *
+     * @param numerator the number of beats per measure
+     * @param denominator the note value representing one beat
+     */
     public TimeSignature(int numerator, int denominator) {
         this.numerator = numerator;
         this.denominator = denominator;
     }
 
+    /**
+     * Gets the number of beats per measure.
+     *
+     * @return the beats per measure
+     */
     public int getBeatsPerMeasure() {
         return numerator;
     }
 
+    /**
+     * Checks if the time signature is compound.
+     *
+     * @return true if the time signature is compound, false otherwise
+     */
     public boolean isCompound() {
         return numerator % 3 == 0;
     }
