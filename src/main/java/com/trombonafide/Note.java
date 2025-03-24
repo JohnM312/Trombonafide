59de883cf56a47541fde3a2f3202c89cfdfddfbb
John Mai <143438343+JohnM312@users.noreply.github.com>
2025-03-24 15:42:04 -0400

    Update Note.java

--- src/main/java/com/trombonafide/Note.java
+++ src/main/java/com/trombonafide/Note.java
@@ -1,11 +1,24 @@
 package com.trombonafide;
 
+/**
+ * Represents a musical note with pitch, octave, duration, and velocity.
+ * 
+ * @author John Mai
+ */
 public class Note {
     private String pitch;
     private int octave;
     private float duration;
     private int velocity;
 
+    /**
+     * Constructs a new Note instance.
+     *
+     * @param pitch the pitch of the note (e.g., "C#", "A")
+     * @param octave the octave in which the note is played
+     * @param duration the duration of the note in beats
+     * @param velocity the velocity (intensity) of the note
+     */
     public Note(String pitch, int octave, float duration, int velocity) {
         this.pitch = pitch;
         this.octave = octave;
@@ -13,16 +26,28 @@ public class Note {
         this.velocity = velocity;
     }
 
+    /**
+     * Plays the note.
+     */
     public void play() {
         // Play the note
     }
 
+    /**
+     * Transposes the note by a given interval.
+     *
+     * @param interval the interval by which to transpose the note
+     */
     public void transpose(int interval) {
         // Adjust pitch by interval
     }
 
+    /**
+     * Gets the frequency of the note.
+     *
+     * @return the frequency in Hz
+     */
     public float getFrequency() {
-        // Calculate frequency of the note
         return 0;
     }
 }
