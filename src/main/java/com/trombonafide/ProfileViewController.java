d3291e36815b77715f528bae1ae098d345e49960
Andrew Lim <a.lim10@icloud.com>
2025-04-27 23:25:48 -0400

    profile desing

--- src/main/java/com/trombonafide/ProfileViewController.java
+++ src/main/java/com/trombonafide/ProfileViewController.java
@@ -1,11 +1,27 @@
 package com.trombonafide;
 
 import java.io.IOException;
+
+import javafx.scene.image.ImageView;
 import com.model.User;
 import com.trombonafide.MusicSystemFacade;
+
+import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 
 public class ProfileViewController {
     @FXML private Label usernameLabel;
@@ -13,9 +29,11 @@ public class ProfileViewController {
     @FXML private Label emailLabel;
     @FXML private Label pointsLabel;
     @FXML private Label lessonsLabel;
+    @FXML private AnchorPane anchorPane;
+    @FXML private ImageView profileImageView;
 
     @FXML
-    private void goBack(ActionEvent event) throws IOException {
+    private void goBack() throws IOException {
         App.setRoot("home");
     }
 
@@ -30,5 +48,25 @@ public class ProfileViewController {
             pointsLabel.setText("Points: " + currentUser.getProgress().getTotalPoints());
             lessonsLabel.setText("Lessons Completed: " + currentUser.getProgress().getCompletedLessons().size());
         }
+
+        Image backgroundImage = new Image(getClass().getResource("/images/profileicon.jpg").toExternalForm());
+
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, false, true);
+        BackgroundImage background = new BackgroundImage(backgroundImage, 
+                                            BackgroundRepeat.NO_REPEAT, 
+                                            BackgroundRepeat.NO_REPEAT, 
+                                            BackgroundPosition.CENTER, 
+                                            backgroundSize);
+        anchorPane.setBackground(new Background(background));
+
+        Rectangle overlay = new Rectangle();
+        overlay.setFill(new Color(0, 0, 0, 0.4));
+        overlay.widthProperty().bind(anchorPane.widthProperty());
+        overlay.heightProperty().bind(anchorPane.heightProperty());
+        anchorPane.getChildren().add(0, overlay);
+
+        Platform.runLater(() -> {
+            anchorPane.getScene().getStylesheets().add(getClass().getResource("/style.css").toExternalForm());
+        });
     }
 }
